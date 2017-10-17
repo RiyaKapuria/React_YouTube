0503eb1cef06248329b6fb9c9a48d80d0c90407b
Riya Kapuria <riya@railsfactory.org>
2017-10-17 10:13:21 +0530

    added all files to the repo

---
+++ src/app/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+// export const Header = (props) => {
+//   return (
+//     <nav className="navbar navbar-default">
+//       <div className="container">
+//         <div className="navbar-header">
+//           <ul className="nav navbar-nav">
+//             <li><a href="#">{props.homeLink}</a></li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+export const Header = (props) => {
+  return (
+    <nav className="navbar navbar-default">
+      <div className="container">
+        <div className="navbar-header">
+          <ul className="nav navbar-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">User</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
